51086638ea3bb5444c21b718de0d6e122598d7ef
Winni Neessen <wn@neessen.dev>
2026-08-26 20:57:13 +0200

    Introduce health check endpoint and enhance server response handling
    - Added a `/healthz` HTTP endpoint to report service health status
    - Implemented reusable JSON response and error rendering structs (`Response`, `ErrorDetail`) in `http_response.go`
    - Introduced application error types (`apperror`) for managing common error cases
    - Configured router in `router.go` to include middleware and foundational routes
    - Activated previously commented-out HTTP routes initialization in `server.go`

---
+++ internal/server/httphandler_healthz.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+
+	"github.com/wneessen/localweather/internal/log"
+)
+
+type HealthzResponse struct {
+	ServiceIsHealthy bool `json:"service_healthy"`
+}
+
+// handlerHealthzGet responds with a JSON payload indicating service health and the current UTC time.
+func (s *Server) handlerHealthzGet(w http.ResponseWriter, r *http.Request) {
+	resp := NewResponse(http.StatusOK, "service is up and running", HealthzResponse{ServiceIsHealthy: true})
+	if err := render.Render(w, r, resp); err != nil {
+		s.log.Error("failed to render healthz JSON", log.ErrAttr(err))
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -68,7 +68,7 @@ func (s *Server) Start(ctx context.Context) error {
 	}
 
 	s.log.Info("starting http backend", "listen_addr", s.conf.ListenAddr())
-	//s.httpRoutes(ctx)
+	s.httpRoutes(ctx)
 	if err := s.httpserv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("failed to start http server: %w", err)
 	}
