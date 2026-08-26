b6ddac7a16b7493824384cf2b90ec7fe7edac4ec
Winni Neessen <wn@neessen.dev>
2026-08-26 15:06:27 +0200

    Introduce configuration package with logging and server settings
    - Added `LogLevel` type with methods for string conversion, unmarshalling, and mapping to `slog.Level`
    - Created `Config` struct to manage application settings for logging, server, and scheduler
    - Implemented `New` method to load configuration using the `fig` library with environment support
    - Added helper method `ListenAddr` to construct the server address from config values

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kkyr/fig"
+)
+
+type Config struct {
+	Log struct {
+		Format string   `fig:"format" default:"json"`
+		Output string   `fig:"output" default:"stdout"`
+		Level  LogLevel `fig:"level" default:"info"`
+	}
+	Server struct {
+		Address        string        `fig:"bindaddr" default:"127.0.0.1"`
+		Port           uint32        `fig:"port" default:"10001"`
+		Timeout        time.Duration `fig:"timeout" default:"5s"`
+		DevEnvironment bool          `fig:"dev_env"`
+	}
+	Scheduler struct {
+		MaintenanceInterval time.Duration `fig:"maintenance_interval" default:"1h"`
+	}
+}
+
+// New creates a new instance of Config by reading and loading configuration values. It takes in the file
+// path and file name of the configuration file as parameters. It returns a pointer to the Config and an
+// error if there was a problem reading or loading the configuration.
+func New(path, file string) (*Config, error) {
+	config := Config{}
+	_, err := os.Stat(fmt.Sprintf("%s/%s", path, file))
+	if err != nil {
+		return &config, fmt.Errorf("failed to read config: %w", err)
+	}
+
+	if err = fig.Load(&config, fig.Dirs(path), fig.File(file), fig.UseEnv("localweather")); err != nil {
+		return &config, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	return &config, nil
+}
+
+// ListenAddr constructs and returns the server address by combining the server's address and port from
+// the configuration.
+func (c *Config) ListenAddr() string {
+	serverAddr := ":" + fmt.Sprintf("%d", c.Server.Port)
+	if c.Server.Address != "" {
+		serverAddr = c.Server.Address + serverAddr
+	}
+	return serverAddr
+}
